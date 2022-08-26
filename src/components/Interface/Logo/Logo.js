c4c3d09995233c26256a44f43236c28462510346
i-zinko <i.zinko@x-way.ru>
2022-08-26 12:29:39 +0300

    Add all files

--- src/components/Interface/Logo/Logo.js
+++ src/components/Interface/Logo/Logo.js
@@ -6,7 +6,7 @@ class Logo extends Component {
         super();
     }
     render() {
-        return <img src='../../../assets/images/stich.jpg' className='logo' alt="logo" />
+        return <img src='https://i.pinimg.com/236x/09/27/d2/0927d2faa1df965115a0df12b8333f5d.jpg' className='logo' alt="logo" />
     }
 }
 
